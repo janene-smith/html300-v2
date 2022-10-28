81daaa3fba4ba1981ba2423761ab18e3eecacc08
Janene Smith <j.smith.peachy.bit@gmail.com>
2022-10-28 08:30:45 -0700

    Working on mixins

--- lesson08/assignment/portfolio/src/mixins/imgToggleMixin.js
+++ lesson08/assignment/portfolio/src/mixins/imgToggleMixin.js
@@ -1,26 +1,40 @@
+import { Modal } from "bootstrap";
+
 // exports imgToggleMixin message to console when the component is created.
 export const imgToggleMixin = {
     created() {
         console.log('Hello from the mixin!')
     },
 
-
-
-
-
 // Commenting out working imgToggleMixin
 
 // export const showHide = {
-    
+const toggle = {  
     data() {
         return {
-            message: 'I am secondary.'
+            message: 'I am secondary.',
+            isShowing: false
         }
     },
     methods: {
-        toggleShowHide() {
-            this.isHidden = !this.isHidden;
+        toggleShow() {
+            this.isShowing = !this.isShowing;
         }
     }
 }
+const Modal = {
+    template: '#modal',
+    mixins: [toggle],
+    components: {
+        appChild: Child
+    }
+};
+
+new Vue ({
+    el: '#app',
+    components: {
+        appModal: Modal,
+        appTooltip: Tooltip
+    }
+});
 
